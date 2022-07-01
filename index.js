9093c32222239c1616feeb6f31a5ddd16d896bc8
hasinsadique <hasinsadique7@gmail.com>
2022-07-02 03:39:03 +0600

    hiii

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ async function run() {
 
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
+app.get("/", async(req, res) => {
     res.send("Hello User Welcome to Task Management App Server");
 });
 
